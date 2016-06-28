b3804332fa4d91a09aa9e39073d07b7922018bc4
Andrew Niekamp and Russell Davies <andrew+russell@>
2016-06-28 13:05:08 -0700

    Add movie name, time, age inputs and create an instance and prototype

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+function Ticket(name, age, time) {
+  this.movieName = name;
+  this.custAge = age;
+  this.movieTime = time;
+  this.price = 12;
+}
+
+Ticket.prototype.adjustedPrice = function() {
+  if (this.custAge === "Senior" || this.custAge === "Child") {
+    this.price += 1;
+    console.log(this.price);
+  }
+  return this.price;
+}
+
+//TEST DATA
+
+
+
+$(function() {
+  $("#movie-titles").on('change', (function() {
+    var movieTime = parseInt($("#movie-titles").find(":selected").val());
+    if (movieTime === 1) {
+      $("#time2-box").hide();
+      $("#time1-box").show();
+    } else if (movieTime === 2) {
+      $("#time1-box").hide();
+      $("#time2-box").show();
+    } else {
+      $("#time1-box").hide();
+      $("#time2-box").hide();
+    }
+  }));
+  $("form").submit(function(event){
+    event.preventDefault();
+    var movieName = parseInt($("#movie-titles").val());
+    var ageCustom = $("input:radio[name=age]:checked").val();
+    if (movieName === 1){
+      var timeEquals = $("#movie-time1").val();
+    } else {
+      var timeEquals = $("#movie-time2").val();
+    }
+    console.log(timeEquals);
+    console.log(movieName);
+    console.log(ageCustom);
+
+    var testMovie = new Ticket(movieName, ageCustom, timeEquals);
+    testMovie.adjustedPrice();
+    console.log(testMovie);
+  });
+});
